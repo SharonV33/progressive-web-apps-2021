480078af28cce5abb2cf6e88b353ba34e0618c14
sharonV33 <sharonveldman33@gmail.com>
2021-03-09 11:07:58 +0100

    get data from api add style sheet

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const request = require('request')
 const app = express()
 
 const hostname = '127.0.0.1'
@@ -9,24 +10,31 @@ app.set('views', 'views')
 
 app.use(express.static('public'))
 
-
 //home page
-app.get('/', (req, res) => {
-    const albums =[
-        {name: 'high voltage', artist: 'ac/dc'},
-        {name: 'danger days', artist: 'MCR'},
-        {name: 'holy hell', artist: 'architects'}
-    ]
-    res.render('pages/index',  {
-        allAlbums: albums
+app.get('/', function(req, res) {
+    request('https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=metalcore&api_key=b0cbd53d2ea5b525c2a0447aa31fcd10&format=json', {json: true}, function (err, requestRes, body){
+        if (err) {
+            // We got an error
+            res.send(err)
+        } else {
+            res.render('pages/index', {
+                allAlbums: body.albums.album
+            })
+        }
     })
 })
 
 //detail page
-app.get('/mbid', function (req, res) {
-    res.send('Detail!')
+app.get('/mbid/:id', function (req, res) {
+    console.log(req.params.id)
+    res.render('pages/album', {
+        title: `Post ${req.params.id}`
+    })
 })
+
+
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 })
+
