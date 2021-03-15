c40983e7f71fed61c7a02f99b3fbcdc4687866d6
sharonV33 <sharonveldman33@gmail.com>
2021-03-15 13:42:53 +0100

    start working on converting the node app into a PWA Add manifest.json

--- app.js
+++ app.js
@@ -10,16 +10,18 @@ app.set('views', 'views')
 
 app.use(express.static('public'))
 
+
 //home page
 app.get('/', function(req, res) {
-    request('https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=metalcore&api_key=b0cbd53d2ea5b525c2a0447aa31fcd10&format=json', {json: true}, function (err, requestRes, body){
+    const genre = "deathcore"
+    request(`https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=${genre}&limit=100&api_key=b0cbd53d2ea5b525c2a0447aa31fcd10&format=json`, {json: true}, function (err, requestRes, body){
         if (err) {
             // We got an error
             res.send(err)
         } else {
-            //clean data
+            //clean data to remove albums without an album id
             const data = body.albums.album.filter((album) => album.mbid)
-
+            //load home page with the data
             res.render('pages/index', {
                 allAlbums: data
             })
@@ -29,13 +31,11 @@ app.get('/', function(req, res) {
 
 //detail page
 app.get('/mbid/:id', function (req, res) {
-    request('https://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid=' + req.params.id + '&api_key=b0cbd53d2ea5b525c2a0447aa31fcd10&format=json', {json: true}, function (err, requestRes, body){
+    request(`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid=${req.params.id}&api_key=b0cbd53d2ea5b525c2a0447aa31fcd10&format=json`, {json: true}, function (err, requestRes, body){
         if (err) {
             // We got an error
             res.send(err)
         } else {
-            //clean data
-
             res.render('pages/album', {
                 albumInfo: body.album,
                 tracks: body.album.tracks.track
@@ -43,7 +43,6 @@ app.get('/mbid/:id', function (req, res) {
         }
     })
 })
-
 
 //the "process.env.PORT" is specific for Heroku deployment
 app.listen(process.env.PORT || port, () => {
