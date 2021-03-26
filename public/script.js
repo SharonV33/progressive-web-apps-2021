26298b1ba1bae0b03a1fb3cb46499be63ea56bd3
sharonV33 <sharonveldman33@gmail.com>
2021-03-26 13:57:30 +0100

    fix stupid heroku

--- public/script.js
+++ public/script.js
@@ -1,8 +1,5 @@
-if ('serviceWorker' in navigator) {
-   window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js')
-          .then((reg) => console.log('service worker registered', reg))
-          .catch((err) => console.log('service worker not registered', err))
-   })
-
+if (!navigator.serviceWorker.controller) {
+   navigator.serviceWorker.register("/sw.js").then(function(reg) {
+      console.log("Service worker has been registered for scope: " + reg.scope);
+   });
 }
